6f8a0db66626a1a56a57d91896d0c125b1076aeb
haris2303 <hrsccf102@gmail.com>
2023-09-09 17:45:34 +0900

    Request

--- routes/web.php
+++ routes/web.php
@@ -72,3 +72,6 @@ Route::get('/produk/{id}', function ($id) {
 Route::get('/produk-redirect/{id}', function ($id) {
     return redirect()->route('product.detail', ['id' => $id]);
 });
+
+Route::get('/controller/hello/request', [App\Http\Controllers\HelloController::class, 'request']);
+Route::get('/controller/hello/{name}', [App\Http\Controllers\HelloController::class, 'hello']);
