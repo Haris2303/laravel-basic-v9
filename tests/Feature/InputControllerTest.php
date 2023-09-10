fb692e48d3331a74eb25e056752b9d1c476b752b
haris2303 <hrsccf102@gmail.com>
2023-09-10 11:54:52 +0900

    Request Input

---
+++ tests/Feature/InputControllerTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class InputControllerTest extends TestCase
+{
+    public function testInput()
+    {
+        $this->get('/input/hello?name=Haris')
+            ->assertSeeText('Hello Haris');
+
+        $this->post('/input/hello', ['name' => 'Haris'])
+            ->assertSeeText('Hello Haris');
+    }
+
+    public function testNestedInput()
+    {
+        $this->post('/input/hello/first', [
+            "name" => [
+                "first" => "Haris"
+            ]
+        ])->assertSeeText('Hello Haris');
+    }
+
+    public function testInputAll()
+    {
+        $this->post('/input/hello/input', [
+            "name" => [
+                "first" => "Haris",
+                "last" => "Aja"
+            ]
+        ])->assertSeeText('name')
+            ->assertSeeText('first')
+            ->assertSeeText('Haris')
+            ->assertSeeText('last')
+            ->assertSeeText('Aja');
+    }
+
+    public function testInputArray()
+    {
+        $this->post('/input/hello/array', [
+            'products' => [
+                [
+                    'name' => 'OPPO A5 2020',
+                    'price' => 2400000
+                ],
+                [
+                    'name' => 'POCO PHONE F5',
+                    'price' => 5000000
+                ]
+            ]
+        ])->assertSeeText('OPPO A5 2020')
+            ->assertSeeText('POCO PHONE F5');
+    }
+}
