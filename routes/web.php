f6bb58733f796fce34c45bc582811ab40c729e9a
haris2303 <hrsccf102@gmail.com>
2023-09-11 18:05:34 +0900

    Route Group

--- routes/web.php
+++ routes/web.php
@@ -96,29 +96,40 @@ Route::post('/file/upload', [App\Http\Controllers\FileController::class, 'upload
 
 Route::get('/response/hello', [ResponseController::class, 'response']);
 Route::get('/response/header', [ResponseController::class, 'header']);
-Route::get('/response/type/view', [ResponseController::class, 'responseView']);
-Route::get('/response/type/json', [ResponseController::class, 'responseJson']);
-Route::get('/response/type/file', [ResponseController::class, 'responseFile']);
-Route::get('/response/type/download', [ResponseController::class, 'responseDownload']);
+
+// Grouping route prefix
+Route::prefix('/response/type')->group(function () {
+    Route::get('/view', [ResponseController::class, 'responseView']);
+    Route::get('/json', [ResponseController::class, 'responseJson']);
+    Route::get('/file', [ResponseController::class, 'responseFile']);
+    Route::get('/download', [ResponseController::class, 'responseDownload']);
+});
 
 Route::get('/cookie/set', [CookieController::class, 'createCookie']);
 Route::get('/cookie/get', [CookieController::class, 'getCookie']);
 Route::get('/cookie/clear', [CookieController::class, 'clearCookie']);
 
-Route::get('/redirect/from', [RedirectController::class, 'redirectFrom']);
-Route::get('/redirect/to', [RedirectController::class, 'redirectTo']);
-Route::get('/redirect/name', [RedirectController::class, 'redirectName']);
-Route::get('/redirect/name/{name}', [RedirectController::class, 'redirectHello'])->name('redirect-hello');
-Route::get('/redirect/action', [RedirectController::class, 'redirectAction']);
-Route::get('/redirect/away', [RedirectController::class, 'redirectAway']);
+// Grouping route controller
+Route::controller(RedirectController::class)->group(function () {
+    Route::get('/redirect/from', 'redirectFrom');
+    Route::get('/redirect/to', 'redirectTo');
+    Route::get('/redirect/name', 'redirectName');
+    Route::get('/redirect/name/{name}', 'redirectHello')->name('redirect-hello');
+    Route::get('/redirect/action', 'redirectAction');
+    Route::get('/redirect/away', 'redirectAway');
+});
 
-Route::get('/middleware/api', function () {
-    return 'OK';
-})->middleware('contoh:UDIN-TAMVAN,401');
+// Grouping routes multiple middleware and prefix
+Route::middleware('contoh:UDIN-TAMVAN,401')->prefix('/middleware')->group(function () {
+    Route::get('/api', function () {
+        return 'OK';
+    });
 
-Route::get('/middleware/group', function () {
-    return 'GROUP';
-})->middleware(['pzn']);
+    Route::get('/group', function () {
+        return 'GROUP';
+    });
+});
+
 
 Route::get('/form', [App\Http\Controllers\FormController::class, 'form']);
 Route::post('/form', [App\Http\Controllers\FormController::class, 'submit']);
